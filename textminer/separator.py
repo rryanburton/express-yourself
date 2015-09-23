eaabd9b68f563f4b44638dadab447a8489e9a0a3
Ryan Burton <rryanburton@gmail.com>
2015-09-22 23:16:23 -0400

    intial commit

---
+++ textminer/separator.py
@@ -0,0 +1,18 @@
+import re
+
+def words():
+    '''
+    takes a single or multi word string and
+    splits it into a list of single words:
+    ['hello', 'world']
+    '''
+    pass
+
+def phone_number(input):
+    '''
+    takes a 10 digit number and breaks it down to:
+     {"area_code": "919", "number": "555-1212"}
+
+    '''
+
+    pass
